74ba986598d14a952f811b79cebf349c40588df2
michal skocovsky <89877219+michsko@users.noreply.github.com>
2023-03-22 11:28:08 +0100

    Update settings.py

--- fteller/fteller/settings.py
+++ fteller/fteller/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*08+n(9p*dp$2rxtbsqk+p0hnm(bm55m6!(w309dz17gsh-$7q'
+SECRET_KEY = 'XXXXXXXXXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
